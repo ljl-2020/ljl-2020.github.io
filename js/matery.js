c9a3a1637ec77baa191570e59c2a91e96359e072
ljl-2020 <2830429738@qq.com>
2020-06-14 21:52:25 +0800

    Site updated: 2020-06-14 21:52:24

--- js/matery.js
+++ js/matery.js
@@ -105,7 +105,10 @@ $(function () {
             // 启用字幕
             subHtmlSelectorRelative: true
         });
-
+        //新添加的
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href",$(this).attr("data-original"));
+        });
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
         if (progressElement) {
